4790cd42de30ec104570609b0beeebabbd72933f
emcia <emciastek@gmail.com>
2020-03-29 13:26:21 +0200

    Add message rest controller

--- backend/src/main/kotlin/com/bodyweightapp/webapi/backend/BackendApplication.kt
+++ backend/src/main/kotlin/com/bodyweightapp/webapi/backend/BackendApplication.kt
@@ -4,9 +4,15 @@ import com.okta.spring.boot.oauth.Okta
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
+import org.springframework.web.bind.annotation.CrossOrigin
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 
 @SpringBootApplication
@@ -31,6 +37,30 @@ class OktaOAuth2WebSecurityConfigurerAdapter : WebSecurityConfigurerAdapter() {
     }
 }
 
+@RestController
+@CrossOrigin(origins = ["http://localhost:8080"])
+class MessageOfTheDayController {
+    @GetMapping("/api/userProfile")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
+    fun getUserDetails(authentication: JwtAuthenticationToken): Map<String, Any> {
+        return authentication.tokenAttributes
+    }
+
+    @GetMapping("/api/messages")
+    @PreAuthorize("hasAuthority('SCOPE_email')")
+    fun messages(): Map<String, Any> {
+        val result: MutableMap<String, Any> = HashMap()
+        result["messages"] = Arrays.asList(
+                Message("I am a robot."),
+                Message("Hello, world!")
+        )
+        return result
+    }
+}
+
+class Message(var text: String) {
+    var date: Date = Date()
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(BackendApplication::class.java, *args)
